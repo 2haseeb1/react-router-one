5a94e4369b2ae194360c217b6bce646820e33e2d
2haseeb1 <prodhanhome@gmail.com>
2023-07-27 03:47:05 +0600

    sixthh  commit nearly finishing first page

--- src/JobsList.jsx
+++ src/JobsList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import jobsDataLoader from "./loader/jobsDataLoader";
-
+import locationImg from './icons/Location Icon.png'
+import salaryImg from './icons/dollar.png'
 function JobsList() {
 
   const [jobs, setJobs] = useState([]);
@@ -17,9 +18,9 @@ function JobsList() {
 
 
   return (
-    <div className="jobs-list grid grid-cols-2 gap-3 mt-5 w-[100%]text-center mx-[12%]">
+    <div className="jobs-list grid md:grid-cols-2 gap-[90px] mt-5 w-[100%]text-center mx-[5%]">
       {jobs.map((job) => (
-        <div className="border border-solid border-gray-800 rounded-md p-3 w-[500px] h-[392px] mr-5 mx-auto mb-[40px]" key={job.id}>
+        <div className="border border-solid border-gray-800 rounded-md p-3 w-[640px] h-[392px] mr-5 mx-auto mb-[40px]mr-[90px]" key={job.id}>
           <img src={job.companylogo} alt={job.companybrandname} />
           <h3 className="text-[24px] font-bold mt-3">{job.profession}</h3>
           <h4 className="text-[20px]">{job.companybrandname}</h4>
@@ -27,7 +28,7 @@ function JobsList() {
             {job.jobtype} | {job.joblocation}
           </p>
           
-          <p className="text-[20px]">{job.salaryrange}</p>
+          <p className="text-[20px] flex mt-1"> <span className="block"><img src={locationImg} alt="" className="block" /> </span> {job.location}   <span><img src={ salaryImg} alt="" className="w-[20px] relative top-2 ml-3 mr-1"/></span> Salary:{job.salaryrange}</p>
         
           <Link
             to={`/job/${job.id}`}
